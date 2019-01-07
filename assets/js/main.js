560b239364b89d35c05f79dcfaedeb22f5fa25e9
Ayodele <a.akinbohun@ecscorpresources.com>
2019-01-07 07:58:28 -0800

    Created and Implemented Basic UIs for Questioner App

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+// var navbar = document.getElementById('navbar');
+// document.addEventListener('scroll',()=>{
+//     if(scrollY>40 ){
+//             navbar.style.position = 'fixed';
+//             navbar.style.left = 0;
+//             navbar.style.right = 0;
+//     console.log('fixed : '+ scrollY)
+//     }else{
+//          if(navbar.style.position !== 'static') {
+//             navbar.style.position = 'static';
+//             console.log('static : ' + scrollY)
+//          } 
+//     }
+// })
